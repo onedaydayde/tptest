e34eb9d0a002974a28d610c886a72e5831f8f7f9
onedaydayde <diannaomi41@gmail.com>
2020-01-25 15:20:42 +0800

    登陆功能

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -2,11 +2,63 @@
 
 namespace app\admin\controller;
 
+use think\Db;
 use think\Controller;
 use think\Request;
+use think\Session;
 
 class Login extends Controller
 {
+    /**
+     * 登陆
+     *
+     * @return \think\Response
+     */
+    public function login()
+    {
+        //一个方法 处理两个业务逻辑：页面展示  表单提交
+        if (request()->isPost()) {
+            //接受全部参数
+            $params = input();
+//            dump($params);
+            //参数检测 （表单验证）
+            $rule = [
+                'username|用户名' => 'require',
+                'password|密码' => 'require',
+                //'code|验证码' => 'require|captcha'
+            ];
+            $res = $this->validate($params, $rule);
+            if ($res !== true) {
+                $this->error($res);
+            }
+            //验证码手动校验
+//            if (!captcha_check($params['code'])) {
+//                $this->error('验证码错误');
+//            }
+            //查询管理员用户表
+            $password = $params['password'];
+            //数据库查询当前用户名的哈希密码
+            $manager=Db::table('tpshop_manager')->where('username',$params['username'])->find();
+            //库中密码与提交密码解密匹配是否为true
+            if (password_verify($password,$manager['password'])) {
+                //登录成功
+                //设置登录标识到session
+                Session::set('name',$manager['username']);
+                Session::set('password',$manager['password']);
+                //页面跳转
+                $this->success('登录成功', 'admin/index/index');
+            } else {
+                //用户名或密码错误
+                $this->error('用户名或密码错误');
+            }
+        } else {
+            //get请求  页面展示
+            //临时关闭全局模板布局
+            $this->view->engine->layout(false);
+            return view();
+        }
+    }
+
     /**
      * 显示资源列表
      *
@@ -15,8 +67,8 @@ class Login extends Controller
     public function index()
     {
         //临时关闭模板布局
-        $this->view->engine->layout(false);
-        return view();
+//        $this->view->engine->layout(false);
+//        return view();
     }
 
     /**
@@ -32,7 +84,7 @@ class Login extends Controller
     /**
      * 保存新建的资源
      *
-     * @param  \think\Request  $request
+     * @param \think\Request $request
      * @return \think\Response
      */
     public function save(Request $request)
@@ -43,7 +95,7 @@ class Login extends Controller
     /**
      * 显示指定的资源
      *
-     * @param  int  $id
+     * @param int $id
      * @return \think\Response
      */
     public function read($id)
@@ -54,7 +106,7 @@ class Login extends Controller
     /**
      * 显示编辑资源表单页.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \think\Response
      */
     public function edit($id)
@@ -65,8 +117,8 @@ class Login extends Controller
     /**
      * 保存更新的资源
      *
-     * @param  \think\Request  $request
-     * @param  int  $id
+     * @param \think\Request $request
+     * @param int $id
      * @return \think\Response
      */
     public function update(Request $request, $id)
@@ -77,7 +129,7 @@ class Login extends Controller
     /**
      * 删除指定资源
      *
-     * @param  int  $id
+     * @param int $id
      * @return \think\Response
      */
     public function delete($id)
