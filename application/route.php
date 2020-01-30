a5d0e8153cc73ed148a51916e9a1b2b2a0edef1a
onedaydayde <diannaomi41@gmail.com>
2020-01-30 21:06:28 +0800

    tp5 初始化

--- application/route.php
+++ application/route.php
@@ -9,84 +9,13 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-use think\Route;
+return [
+    '__pattern__' => [
+        'name' => '\w+',
+    ],
+    '[hello]'     => [
+        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+        ':name' => ['index/hello', ['method' => 'post']],
+    ],
 
-//后台接口域名路由 adminapi
-Route::domain('adminapi', function(){
-    //adminapi模块首页路由
-    Route::get('/', 'adminapi/index/index');
-    //定义 域名下的其他路由
-    //比如 以后定义路由 get请求  http://adminapi.pyg.com/goods  访问到 adminapi模块Goods控制器index方法
-    //Route::resource('goods', 'adminapi/goods');
-    //获取验证码接口
-    Route::get('captcha/:id', "\\think\\captcha\\CaptchaController@index");
-    Route::get('captcha', 'adminapi/login/captcha');
-    //登录接口
-    Route::post('login', 'adminapi/login/login');
-    //退出接口
-    Route::get('logout', 'adminapi/login/logout');
-    //权限接口
-    Route::resource('auths', 'adminapi/auth', [], ['id'=>'\d+']);
-    //查询菜单权限的接口
-    Route::get('nav', 'adminapi/auth/nav');
-    //角色接口
-    Route::resource('roles', 'adminapi/role', [], ['id'=>'\d+']);
-    //管理员接口
-    Route::resource('admins', 'adminapi/admin', [], ['id'=>'\d+']);
-    //商品分类接口
-    Route::resource('categorys', 'adminapi/category', [], ['id'=>'\d+']);
-
-    //单图片上传接口
-    Route::post('logo', 'adminapi/upload/logo');
-    //多图片上传接口
-    Route::post('images', 'adminapi/upload/images');
-    //商品品牌接口
-    Route::resource('brands', 'adminapi/brand', [], ['id'=>'\d+']);
-    //商品模型（类型）接口
-    Route::resource('types', 'adminapi/type', [], ['id'=>'\d+']);
-    //商品接口
-    Route::resource('goods', 'adminapi/goods', [], ['id'=>'\d+']);
-    //相册图片删除
-    Route::delete('delpics/:id', 'adminapi/goods/delpics', [], ['id'=>'\d+']);
-});
-
-//后台接口域名路由 adminapi
-Route::domain('admin', function(){
-    //admin模块首页路由
-    Route::get('/', 'admin/index/index');
-    //定义 域名下的其他路由
-    //比如 以后定义路由 get请求  http://adminapi.pyg.com/goods  访问到 adminapi模块Goods控制器index方法
-    //Route::resource('goods', 'adminapi/goods');
-    //获取验证码接口
-    Route::get('captcha/:id', "\\think\\captcha\\CaptchaController@index");
-    Route::get('captcha', 'adminapi/login/captcha');
-    //登录页面显示
-    Route::get('login', 'admin/login/index');
-    //登录接口
-    Route::post('read', 'admin/login/read');
-    //退出接口
-    Route::any('logout', 'admin/login/logout');
-//    //权限接口
-//    Route::resource('auths', 'adminapi/auth', [], ['id'=>'\d+']);
-//    //查询菜单权限的接口
-//    Route::get('nav', 'adminapi/auth/nav');
-//    //角色接口
-//    Route::resource('roles', 'adminapi/role', [], ['id'=>'\d+']);
-//    //管理员接口
-//    Route::resource('admins', 'adminapi/admin', [], ['id'=>'\d+']);
-//    //商品分类接口
-//    Route::resource('categorys', 'adminapi/category', [], ['id'=>'\d+']);
-//
-//    //单图片上传接口
-//    Route::post('logo', 'adminapi/upload/logo');
-//    //多图片上传接口
-//    Route::post('images', 'adminapi/upload/images');
-//    //商品品牌接口
-//    Route::resource('brands', 'adminapi/brand', [], ['id'=>'\d+']);
-//    //商品模型（类型）接口
-//    Route::resource('types', 'adminapi/type', [], ['id'=>'\d+']);
-//    //商品接口
-//    Route::resource('goods', 'adminapi/goods', [], ['id'=>'\d+']);
-//    //相册图片删除
-//    Route::delete('delpics/:id', 'adminapi/goods/delpics', [], ['id'=>'\d+']);
-});
+];
